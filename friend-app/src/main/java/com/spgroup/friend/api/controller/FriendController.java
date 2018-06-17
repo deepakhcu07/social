b5d2558c4b46a2e97b582afedd0442172856c41b
Deepak <deepak.hcu07@gmail.com>
2018-06-17 12:16:51 +0800

    Added Swagger API for Documentation

--- friend-app/src/main/java/com/spgroup/friend/api/controller/FriendController.java
+++ friend-app/src/main/java/com/spgroup/friend/api/controller/FriendController.java
@@ -1,13 +1,28 @@
 package com.spgroup.friend.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/friends")
+@Api(value="Friend Management API",tags= {"Friend Management API"})
 public class FriendController {
 	
-	@RequestMapping("/test")
+	@ApiOperation(value = "Create a new Friend Connection", response = String.class)
+	@ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Tested Successfully"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 500, message = "Internal Server Error")
+    }
+    )
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
 		return "Tested Successfully";
 	}
