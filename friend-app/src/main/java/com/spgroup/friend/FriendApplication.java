ec21460f9418b445f5a7318e4a7d070374d74bbd
Deepak <deepak.hcu07@gmail.com>
2018-06-17 12:32:37 +0800

    Created User Entity

--- friend-app/src/main/java/com/spgroup/friend/FriendApplication.java
+++ friend-app/src/main/java/com/spgroup/friend/FriendApplication.java
@@ -2,8 +2,10 @@ package com.spgroup.friend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class FriendApplication {
 	public static void main(String []args) {
 		SpringApplication.run(FriendApplication.class, args);
